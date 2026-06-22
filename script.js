74f4bd644157d018a0b2c02e82302002c75352bb
nazarpetrushin9 <nazarpetrushin9@gmail.com>
2026-06-22 10:57:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark');
+    this.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
+});
